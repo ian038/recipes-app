125c74f9cc59f1e7b391c8be996aea69606ea18c
ian038 <ianphua3@gmail.com>
2021-11-22 13:16:09 -0500

    Completed error page layout

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Layout from "../components/Layout"
 
 export default function Error() {
     return (
-        <div>
-            Error 404 page
-        </div>
+        <Layout>
+        {/* <SEO title="Error" /> */}
+            <main className="error-page">
+            <section>
+                <h1>404</h1>
+                <h3>page not found</h3>
+            </section>
+            </main>
+        </Layout>
     )
 }
--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import Layout from "../components/Layout"
+import { graphql, Link } from "gatsby"
+import setupTags from "../utils/setupTags"
+import slugify from "slugify"
 
-export default function tags() {
+export default function tags({ data }) {
+    const newTags = setupTags(data.allContentfulRecipe.nodes)
+
     return (
         <Layout>
-            Tags
+            <main className="page">
+                <section className="tags-page">
+                {newTags.map((tag, index) => {
+                    const [text, value] = tag
+                    const slug = slugify(text, { lower: true })
+
+                    return (
+                    <Link to={`/tags/${slug}`} key={index} className="tag">
+                        <h5>{text}</h5>
+                        <p>{value} recipe</p>
+                    </Link>
+                    )
+                })}
+                </section>
+            </main>
         </Layout>
     )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
